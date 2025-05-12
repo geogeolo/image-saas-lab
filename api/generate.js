de285bb345a934376a0a5baeed72f5a82343ff48
geogeolo <163122629+geogeolo@users.noreply.github.com>
2025-05-13 04:17:11 +0800

    Create generate.js

---
+++ api/generate.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const { prompt } = req.body;
+  const hf_token = process.env.HUGGINGFACE_TOKEN;
+
+  const response = await fetch("https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${hf_token}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ inputs: prompt })
+  });
+
+  const blob = await response.blob();
+  const buffer = await blob.arrayBuffer();
+  res.setHeader("Content-Type", "image/png");
+  res.send(Buffer.from(buffer));
+}
