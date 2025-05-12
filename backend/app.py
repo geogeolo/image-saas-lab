e582815f98cedb4aa4ec40e7b4769d55490d904c
geogeolo <163122629+geogeolo@users.noreply.github.com>
2025-05-13 06:04:43 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,4 +17,4 @@ def tts():
         return send_file(fp.name, mimetype="audio/mpeg")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
