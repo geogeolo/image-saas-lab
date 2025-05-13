9287cf643c1bcafa428b7b55755c537a109526d1
geogeolo <163122629+geogeolo@users.noreply.github.com>
2025-05-13 08:38:07 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function translateText(text, targetLangCode) {
   try {
-    const res = await fetch("https://libretranslate.let.rip/translate", {
+    const res = await fetch("https://libretranslate.de/translate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
